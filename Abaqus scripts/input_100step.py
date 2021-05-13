cecf2c43fe9581736ed9d9d3d22949871e4fcb6a
EMills681 <84134315+EMills681@users.noreply.github.com>
2021-05-13 13:57:46 +0100

    Update input_100step.py

--- Abaqus scripts/input_100step.py
+++ Abaqus scripts/input_100step.py
@@ -1,4 +1,8 @@
 # -*- coding: mbcs -*-
+# Creates an Abaqus database for a single gear tooth (1:1 gear ratio) with boundary conditions applied to the meshing surface
+#### Open Abaqus - set the work directory to the location where these python files are stored (file -> set work directory...)
+#### Run this script in Abaqus (file -> run script... -> input_100step)
+
 import geofun
 import flashtfun
 import math
